bee5588c23da19405fdc08db46f0187b98f950bd
aragakerubo <aragakerubo@gmail.com>
2024-07-13 16:58:42 -0400

    Chapter 15: Adding Authentication

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -3,12 +3,11 @@ import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import { signOut } from '@/auth';
-import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 
 
 export default function SideNav() {
-  const router = useRouter();
 
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
@@ -27,7 +26,7 @@ export default function SideNav() {
           'use server';
 
           await signOut();
-          router.push('/');
+          redirect('/login');
         }}>
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
