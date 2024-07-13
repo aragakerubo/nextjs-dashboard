095383cf5a461d031475c365d587c30b7b7e0e1f
aragakerubo <aragakerubo@gmail.com>
2024-07-13 16:09:08 -0400

    Chapter 15: Adding Authentication

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -3,7 +3,7 @@ import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import { signOut } from '@/auth';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 
 
